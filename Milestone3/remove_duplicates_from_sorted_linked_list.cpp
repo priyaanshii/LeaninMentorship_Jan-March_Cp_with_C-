6042ec3b12e79555ce9802332e43b10940217f80
priyaanshii <91135853+priyaanshii@users.noreply.github.com>
2022-03-06 16:39:52 +0530

    Create remove_duplicates_from_sorted_linked_list.cpp

---
+++ Milestone3/remove_duplicates_from_sorted_linked_list.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+     ListNode* p1 = head;
+        while(p1 && p1->next){
+            if(p1->next->val == p1->val){
+                p1->next = p1->next->next;
+            }
+            else{
+                p1 = p1->next;
+            }
+        }
+        return head;
+    
+    }
+};
